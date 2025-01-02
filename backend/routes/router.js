e33c0ed8246dad81c68fb86518c178c9c382ace0
surendra <iampurisurendra@gmail.com>
2025-01-02 17:43:00 +0530

    Create two APIs: the first one is POST and the second one is GET. The testing is done and working, but the controllers are still incomplete.

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,9 @@
 import express from "express";
+import controllers from "../controllers/controllers.js";
 
 const router = express.Router();
+
+router.post("/api/create", controllers.create_user);
+router.get("/api/get", controllers.get_user);
 
 export default router;
