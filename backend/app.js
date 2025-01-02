c94b891529fc3195dd5c0fb87fe65a1230f2bd3e
surendra <iampurisurendra@gmail.com>
2025-01-02 17:07:33 +0530

    Set up the backend and started the server.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import router from "./route/router.js";
+import router from "./routes/router.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
